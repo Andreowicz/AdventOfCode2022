b5a8434fbb9feea6a8a9fd3408c38eed14dea5ef
Andreowicz <ande2190@gmail.com>
2022-12-09 17:26:50 +0100

    day 06 part 1

--- 2022/06/solution.py
+++ 2022/06/solution.py
@@ -2,8 +2,18 @@ f = open(r"c:\Users\u-45886\AdventOfCode2022\2022\06\input", "r")
 sequence = f.read()
 
 proof = []
+sol = 0
+i = 1
 for c in sequence:
     if c in proof:
         proof = [c]
-    el
+    else:
+        proof.append(c)
+    if len(proof) == 4:
+        print(proof)
+        sol = i
+        break
+    i += 1
+
+print(sol-1)
     
