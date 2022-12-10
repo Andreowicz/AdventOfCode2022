03a0dcb16e2aa211215f7308d53a8a10a5db9e8e
Andreowicz <ande2190@gmail.com>
2022-12-10 20:58:28 +0100

    solve day 06

--- 2022/06/solution.py
+++ 2022/06/solution.py
@@ -6,14 +6,16 @@ sol = 0
 i = 1
 for c in sequence:
     if c in proof:
-        proof = [c]
+        index = proof.index(c)
+        proof = proof[index+1:]
+        proof.append(c)
     else:
         proof.append(c)
-    if len(proof) == 4:
+    if len(proof) == 14:
         print(proof)
         sol = i
         break
     i += 1
 
-print(sol-1)
+print(sol)
     
